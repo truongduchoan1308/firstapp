3d8633339017c2e37d78ad26536aea45d6495445
truongduchoan1308 <69799514+truongduchoan1308@users.noreply.github.com>
2021-01-21 17:28:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="wraper">
+          <div class="left-box">
+            <img class="img-avt" src="1999.jpg"></img>
+            <p class="hello">Hello</p>
+            <p class="name">My name is ...</p>
+          </div>
+          <div class="right-box">
+            <p class="text-right-one">Introducing Yourseft Self</p>
+            <p class="text-right-two"> Introduction with Text Boxes</p>
+            <div class="gachngang"></div>
+            <div class="boxes">
+              <div class="box1">
+                <p class="title">TextHere 01</p>
+                <p class="content">There slide is 100% editable. Adapt it to your needs and capture your audience's attention.</p>
+              </div>
+              <div class="box1">
+                <p class="title">TextHere 03</p>
+                <p class="content">There slide is 100% editable. Adapt it to your needs and capture your audience's attention.</p>
+              </div>
+              <div class="box1">
+                <p class="title">TextHere 02</p>
+                <p class="content">There slide is 100% editable. Adapt it to your needs and capture your audience's attention.</p>
+              </div>
+              <div class="box1">
+                <p class="title">TextHere 04</p>
+                <p class="content">There slide is 100% editable. Adapt it to your needs and capture your audience's attention.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
